1bb5ccd9234c9c0570da7504169f252df3954251
xenocubium <dravenmonti@gmail.com>
2023-10-06 18:51:14 -0400

    Remove claim system

--- lib/src/main/java/net/xenocubium/derver/main/Main.java
+++ lib/src/main/java/net/xenocubium/derver/main/Main.java
@@ -1,15 +1,8 @@
 package net.xenocubium.derver.main;
 
-import net.xenocubium.derver.claim.ClaimChunk;
-import net.xenocubium.derver.claim.GetPower;
-import net.xenocubium.derver.claim.Power;
 import net.xenocubium.derver.gen.BiomeGen;
 import net.xenocubium.derver.gen.ChunkGen;
 import net.xenocubium.derver.gen.WorldGen;
-import net.xenocubium.derver.group.AddGroup;
-import net.xenocubium.derver.group.GroupInfo;
-import net.xenocubium.derver.group.LeaveGroup;
-import net.xenocubium.derver.group.NewGroup;
 
 import org.bukkit.Bukkit;
 import org.bukkit.generator.BiomeProvider;
@@ -24,13 +17,6 @@ public class Main extends JavaPlugin {
     public void onEnable() {    	
         Bukkit.getPluginManager().registerEvents(new MainListener(), this);
     	mainGen.renderImage();
-    	this.getCommand("getpower").setExecutor(new GetPower());
-    	this.getCommand("power").setExecutor(new Power());
-    	this.getCommand("claim").setExecutor(new ClaimChunk());
-    	this.getCommand("newgroup").setExecutor(new NewGroup());
-    	this.getCommand("addgroup").setExecutor(new AddGroup());
-    	this.getCommand("leavegroup").setExecutor(new LeaveGroup());
-    	this.getCommand("group").setExecutor(new GroupInfo());
     }
 
     @Override
--- lib/src/main/java/net/xenocubium/derver/main/MainListener.java
+++ lib/src/main/java/net/xenocubium/derver/main/MainListener.java
@@ -1,36 +1,12 @@
 package net.xenocubium.derver.main;
-import java.util.List;
-
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.World.Environment;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.ExplosionPrimeEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 
-import io.papermc.paper.event.player.AsyncChatEvent;
-import net.kyori.adventure.text.Component;
-import net.xenocubium.derver.claim.ChunkThing;
-import net.xenocubium.derver.claim.PowerThing;
-import net.xenocubium.derver.group.ChatThing;
-import net.xenocubium.derver.group.Group;
-
 public class MainListener implements Listener {
-	
-	@EventHandler
-	public void onExplode(ExplosionPrimeEvent event) {
-		event.setCancelled(true);
-	}
-	
+
 	@EventHandler
 	public void onPlayerPortal(PlayerPortalEvent event) {
 		Location from = event.getFrom();
@@ -47,92 +23,4 @@ public class MainListener implements Listener {
 		
 		event.setTo(to);
 	}
-	
-	public boolean blockHandler(BlockEvent event, Player player) {
-		Location loc = event.getBlock().getLocation();
-
-		if (loc.getWorld().getEnvironment() != World.Environment.NORMAL) return false;
-		int x = loc.getChunk().getX();
-		int z = loc.getChunk().getZ();
-		 
-		Group groupObj = (new Group());
-		ChunkThing cThing = new ChunkThing(groupObj);
-		
-		String group = cThing.getChunk(x,z);
-		
-		if (group == null) return false;
-		
-		List<String> groups = groupObj.getGroups(player, false);
-		
-		if (!groups.contains(group)) {
-			player.sendMessage(Component.text("This block is owned by " + group));
-			return true;
-		}
-		
-		return false;
-	}
-	
-	
-	@EventHandler
-	public void onBlockBreak(BlockBreakEvent event) {
-		Player player = event.getPlayer();
-		if (blockHandler(event,player)) {
-			event.setCancelled(true);
-		}
-	 }
-	 
-	 @EventHandler
-	 public void onBlockPlace(BlockPlaceEvent event) {
-		 Player player = event.getPlayer();
-		 if (blockHandler(event,player)) {
-			 event.setCancelled(true);
-		 }
-	 }
-	 
-     @EventHandler
-     public void onPlayerChat(AsyncChatEvent e){
-    	 e.renderer(new ChatThing());
-     }
-     
-     @EventHandler
-     public void onKill(PlayerDeathEvent d) {
-     	
-    	Player target = d.getPlayer();
-    	Entity killer = ((LivingEntity) target).getKiller();
-    	 
-    	Group groupObj = (new Group());
-     	List<String> groups = groupObj.getGroups(target, false);
-     	
-     	if (groups.size() < 1 || groups == null) return;
-     	
-     	PowerThing pThing = new PowerThing(groupObj);
-     	
-     	String group = groups.get(0);
-     	
-     	int amount = pThing.getPower(group);
-     	 
-     	target.sendMessage(Component.text("You lost power from dying"));
-     	
-     	int deduct = (killer == null) ? 1 : 5;
-     	
-     	if (deduct == 1 && amount < 2) return;
-     	
-     	pThing.addPower(group, -deduct);
-     	
-     	if (amount < deduct) {
-     		target.sendMessage(Component.text("Your group ran out of power and collapsed"));
-     		return;
-     	}
-     	
-     	if (killer == null) return;
-     	
-     	Player killerPlayer = (Player) killer;
-     	
-     	groups = groupObj.getGroups(killerPlayer, false);
-     	group = groups.get(0);
-     	
-     	pThing.addPower(group, deduct);
-
-     	killerPlayer.sendMessage(Component.text("You gained power from killing another player"));
-     }
 }
